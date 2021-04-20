0779fe3dcf70d1da6cd400c40ea597323335d517
quang.hoang <hoangvinhquang993@gmail.com>
2021-04-20 15:53:21 +0700

    Initial commit

---
+++ src/main/java/au/com/nab/smartchoice/productpriceservice/dto/entity/ProductPriceEntity.java
@@ -0,0 +1,32 @@
+package au.com.nab.smartchoice.productpriceservice.dto.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "product_price")
+public class ProductPriceEntity {
+
+    @Column(name = "product_id")
+    private String productId;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "base_price")
+    private Long basePrice;
+
+    @Column(name = "promotion")
+    private String promotion;
+
+    @Column(name = "synced_at")
+    private LocalDateTime syncedAt;
+
+
+
+}
---
+++ src/test/java/au/com/nab/smartchoice/productpriceservice/ProductPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.smartchoice.productpriceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
