20a0943f34059abec262dd61532f6268d1eef5e0
nightlonely7 <Speedup79>
2021-05-03 05:47:06 +0700

    minor update

--- src/main/java/au/com/nab/smartchoice/productpriceservice/input/scheduler/ProductPriceSyncScheduler.java
+++ src/main/java/au/com/nab/smartchoice/productpriceservice/input/scheduler/ProductPriceSyncScheduler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ProductPriceTikiSyncScheduler {
+public class ProductPriceSyncScheduler {
 
     private final ProductPriceService productPriceService;
 
